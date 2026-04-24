a04fa2e0df9142f385570a8e4f25e6d9477e40fa
vincentbenito <94896928+vincentbenito@users.noreply.github.com>
2026-04-24 14:48:15 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Basic Service Worker to enable PWA Installation on Mobile
+const CACHE_NAME = 'win-chms-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cache hit or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
